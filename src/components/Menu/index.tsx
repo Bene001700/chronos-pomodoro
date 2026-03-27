5c080e424c8fd9cc1f12f51bb5737f3f27e5fb96
Ebenezer Silva <ebenezersilva7@gmail.com>
2026-03-27 17:42:27 -0300

    Adicionado o theme light no projeto

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,58 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
-import styles from './styles.module.css';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
+import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+
+type AvaliableThemes = "light" | "dark";
+
 export function Menu() {
-    return (
-        <nav className={styles.menu}>
-            <a href="#" className={styles.menuLink}>
-                <HouseIcon/>
-            </a> 
-            <a href="#" className={styles.menuLink}>
-                <HistoryIcon/>
-            </a> 
-            <a href="#" className={styles.menuLink}>
-                <SettingsIcon/>
-            </a> 
-            <a href="#" className={styles.menuLink}>
-                <SunIcon/>
-            </a> 
-        </nav>
-    );    
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem("theme") as
+      | AvaliableThemes
+      | "dark";
+    return storedTheme;
+  });
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      localStorage.setItem("theme", nextTheme);
+      return nextTheme;
+    });
+  }
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <nav className={styles.menu}>
+      <a href="#" className={styles.menuLink}>
+        <HouseIcon />
+      </a>
+      <a href="#" className={styles.menuLink}>
+        <HistoryIcon />
+      </a>
+      <a href="#" className={styles.menuLink}>
+        <SettingsIcon />
+      </a>
+      <a href="#" className={styles.menuLink} onClick={handleThemeChange}>
+        {nextThemeIcon[theme]}
+      </a>
+    </nav>
+  );
 }
