e2d9dc878ea954a60cd89d72d8388cfbdf164e70
Ebenezer Silva <ebenezersilva7@gmail.com>
2026-04-03 16:28:36 -0300

    Adicionado duração no model da task

--- src/contexts/TaskContext/initialTaskState.ts
+++ src/contexts/TaskContext/initialTaskState.ts
@@ -3,7 +3,7 @@ import type { TaskStateModel } from "../../models/TaskStateModel";
 export const initialTaskState: TaskStateModel = {
   tasks: [],
   secondsRemaining: 0,
-  formattedRemaining: "25:00",
+  formattedRemaining: "00:00",
   activeTask: null,
   currentCycle: 0,
   config: {
